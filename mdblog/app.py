fffb95d4ece4b2212096f5f4586aeeb03352b825
FilipKaluza <kaluzafilip33@gmail.com>
2020-08-30 12:47:33 +0000

    render system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from .database import articles
 
 flask_app = Flask(__name__)
 
@@ -18,8 +18,13 @@ def view_admin():
 
 @flask_app.route('/articles/')
 def view_articles():
-    return render_template("articles.jinja")
+    return render_template("articles.jinja", articles=articles.items())
 
-
+@flask_app.route('/articles/<int:art_id>/')
+def view_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.jinja", article=article)
+    return render_template("article_not_found.jinja", art_id=art_id)
 
 
