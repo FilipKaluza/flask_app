05c1938510b6a71664dfc553295994644c57178e
FilipKaluza <kaluzafilip33@gmail.com>
2020-08-30 11:24:04 +0000

    Initial application

---
+++ mdblog/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/')
+def view_welcome_page():
+    return render_template("welcome_page.jinja")
+
+@flask_app.route('/about/')
+def view_about():
+    return render_template("about.jinja")
+
+
+
---
+++ run.py
@@ -0,0 +1,6 @@
+from mdblog.app import flask_app
+
+if __name__ == "__main__":
+    debug = False
+    host = "0.0.0.0"
+    flask_app.run(host, debug = True)
